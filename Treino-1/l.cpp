405abd033b17aa4fef3633fa6ed07cdda7a0abe2
oregonyuky <felipe.akio.nishimura@gmail.com>
2026-08-16 23:49:52 -0300

    4

--- Treino-1/l.cpp
+++ Treino-1/l.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int mxN = 1e5+1;
-int main(){
-    int n, m, a, b; 
-    cin >> n >> m;   
-    for(int i=0;i<n;i++){
-        cin >> a >> b;
-        
+struct cmp{
+    bool operator()(const pair<int,int>&a, const pair<int,int>&b){
+        if(a.first != b.first)
+            return a.first > b.first;
+        return a.second < b.second;
     }
+};
+void teste(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    #endif
+    priority_queue<pair<int, int>, vector<pair<int,int>>, cmp> pq;
+    int n, m, ai, bi, res=0;
+    cin >> n >> m;
+    while(n--){
+        cin >> ai >> bi;
+        pq.push({ai, bi});
+        if(!pq.empty()){
+            res += pq.top();
+            pq.pop();
+        }
+    }
+}
+int main(){
+    teste();
 }
